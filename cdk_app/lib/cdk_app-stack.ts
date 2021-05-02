5fa113c702f2408196ecf201177a06437577f18f
Sadiya-sheik <sadiya.sheikh@atmecs.com>
2021-05-02 22:34:04 +0530

    first change

---
+++ cdk_app/lib/cdk_app-stack.ts
@@ -0,0 +1,36 @@
+import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as s3Deploy from '@aws-cdk/aws-s3-deployment';
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+
+export class CdkAppStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    // S3
+    const bucket = new s3.Bucket(this, "CreateReactAppBucket", {
+      publicReadAccess: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      websiteIndexDocument: "index.html"
+    });
+
+    // Deployment
+    const src = new s3Deploy.BucketDeployment(this, "DeployCRA", {
+      sources: [s3Deploy.Source.asset("../build")],
+      destinationBucket: bucket
+    });@aws-cdk/aws-s3 @aws-cdk/aws-s3-deployment @aws-cdk/aws-cloudfront
+
+    // Cloudfront
+    const cf = new cloudfront.CloudFrontWebDistribution(this, "CDKCRAStaticDistribution", {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: bucket
+          },
+          behaviors: [{isDefaultBehavior: true}]
+        },
+      ]
+    });
+  }
+}
